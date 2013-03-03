0d54f34764bee498db35506af2a60f9503ac58bf
Tok <lsteiger4@gmail.com>
2013-03-03 19:38:31 +0100

    Server side default line width set from 72 to 68 characters.

--- src/main/scala/jenkem/server/ConversionSettings.scala
+++ src/main/scala/jenkem/server/ConversionSettings.scala
@@ -7,7 +7,7 @@ import jenkem.shared.ColorScheme
 import jenkem.shared.Power
 
 class ConversionSettings {
-  var width = 72
+  var width = 68
   var method = ConversionMethod.SuperHybrid
   var scheme = ColorScheme.Default
   var charset = CharacterSet.Hard.getCharacters
