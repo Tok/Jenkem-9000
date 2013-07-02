127bcb3e5bb527c7f4be1fb630cc93680c9b1485
lsteiger4@gmail.com <lsteiger4@gmail.com>
2013-07-02 12:03:30 +0200

    Events tested.

--- src/test/scala/jenkem/event/EventSuite.scala
+++ src/test/scala/jenkem/event/EventSuite.scala
@@ -8,29 +8,43 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class EventSuite extends AbstractTester {
   test("Do Conversion Event") {
+    def testDoConversionEvent(event: DoConversionEvent): Unit = {
+      assert(event.isInstanceOf[DoConversionEvent])
+      assert(event.isInstanceOf[EventObject])
+      assert(event.getSource.isInstanceOf[Object])
+      assert(event.toString === "jenkem.event.DoConversionEvent[source=()]")
+    }
     val falseFalseEvent = new DoConversionEvent(false, false)
-    assert(falseFalseEvent.isInstanceOf[DoConversionEvent])
-    assert(falseFalseEvent.isInstanceOf[EventObject])
+    assert(!falseFalseEvent.prepareImage)
+    assert(!falseFalseEvent.resize)
+    testDoConversionEvent(falseFalseEvent)
     val falseTrueEvent = new DoConversionEvent(false, true)
-    assert(falseTrueEvent.isInstanceOf[DoConversionEvent])
-    assert(falseTrueEvent.isInstanceOf[EventObject])
+    assert(!falseTrueEvent.prepareImage)
+    assert(falseTrueEvent.resize)
+    testDoConversionEvent(falseTrueEvent)
     val trueFalseEvent = new DoConversionEvent(true, false)
-    assert(trueFalseEvent.isInstanceOf[DoConversionEvent])
-    assert(trueFalseEvent.isInstanceOf[EventObject])
+    assert(trueFalseEvent.prepareImage)
+    assert(!trueFalseEvent.resize)
+    testDoConversionEvent(trueFalseEvent)
     val trueTrueEvent = new DoConversionEvent(true, true)
-    assert(trueTrueEvent.isInstanceOf[DoConversionEvent])
-    assert(trueTrueEvent.isInstanceOf[EventObject])
+    assert(trueTrueEvent.prepareImage)
+    assert(trueTrueEvent.resize)
+    testDoConversionEvent(trueTrueEvent)
   }
 
   test("Save Image Event") {
     val event = new SaveImageEvent
     assert(event.isInstanceOf[SaveImageEvent])
     assert(event.isInstanceOf[EventObject])
+    assert(event.getSource.isInstanceOf[Object])
+    assert(event.toString === "jenkem.event.SaveImageEvent[source=()]")
   }
 
   test("Send To IRC Event") {
     val event = new SendToIrcEvent
     assert(event.isInstanceOf[SendToIrcEvent])
     assert(event.isInstanceOf[EventObject])
+    assert(event.getSource.isInstanceOf[Object])
+    assert(event.toString === "jenkem.event.SendToIrcEvent[source=()]")
   }
 }
